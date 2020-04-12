43a3a01b76bbc6a41595c86b65377bbe317c153c
yuma-corder <gh8a2lk14@gmail.com>
2020-04-12 14:58:24 +0000

    [start&Finish]3-9

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
 
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
+
 end
