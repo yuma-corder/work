d88803d97539df0706aa601c2588a598f1bbddea
yuma-corder <gh8a2lk14@gmail.com>
2020-04-23 07:21:37 +0000

    [start&Finish]5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'db9ed4fee74d752b771317d40c7463a3de5590e430469a7a6193897d19646c4a84a9f895994adfbb2b8be934210b27072934b7d41d2e33adfd0f2393c5b4a42a'
